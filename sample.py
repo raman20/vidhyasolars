0ee28a2da924f47a336082d47eff67688a7e163d
RAMAN DWIVEDI <ramandwivedi20@protonmail.com>
2020-08-15 12:21:01 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,39 +1,78 @@
-from flask import Flask,render_template,request
 import os
-import sqlite3 as sq
+from wsgiref.simple_server import make_server
+from jinja2 import Environment, FileSystemLoader
+from cgi import parse_qs,escape
+import sqlite3
 
-app = Flask(__name__)
+class application:
+	def __init__(self):
+		self.routes = {}
 
-@app.route('/',methods=['GET','POST'])
-def home():
-	if request.method == 'GET':
-		return render_template('index.html')
-	elif request.method == 'POST':
-		name = request.form.get('name')
-		email = request.form.get('email')
-		if not email:
+	def __call__(self,environ,start_response):
+		response,status = self.handle_request(environ)
+		response_headers = [('Content-type','text/html'),]
+		start_response(status,response_headers)
+		return [response.encode('utf-8')]
+
+	def handle_request(self,environ):
+		path = environ.get('PATH_INFO')
+		if self.routes.get(path):
+			status = '200 OK'
+			handler = self.routes.get(path)
+			response = handler(environ)
+			return response,status
+		else:
+			status = '404 Not Found'
+			response = environ.get('PATH_INFO')
+			return '<html><h1>error</h1>\n<h2>'+response+'</h2><h3>url not exist</h3><img src="https://bit.ly/2ThsRlS"></html>',status
+
+	def add_handlers(self,path,handler):
+		self.routes[path] = handler
+	
+app = application()
+
+
+def home(environ):
+	method = environ.get("REQUEST_METHOD")
+	temp_env = Environment(loader = FileSystemLoader(os.path.abspath('template')))
+	
+	if method == 'GET':
+		return temp_env.get_template('index.html').render()
+	elif method == 'POST':
+		request_body_size = int(environ.get('CONTENT_LENGTH',0))
+		request_body = environ['wsgi.input'].read(request_body_size)
+		d = parse_qs(request_body)
+		
+		name = d.get(b'name')[0].decode('utf-8')
+		if d.get(b'email'):
+			email = d.get(b'email')[0].decode('utf-8')
+		else:
 			email = 'no email'
-		bill = request.form.get('bill')
-		if not bill:
+		if d.get(b'bill'):
+			bill = d.get(b'bill')[0].decode('utf-8')
+		else:
 			bill = 'no bill'
-		phone = request.form.get('phone')
+		phone = d.get(b'phone')[0].decode('utf-8')
 		date = 'current_date'
 
 		print(name,email,bill,phone,date)
 
-		con = sq.connect('database.db')
+
+		con = sqlite3.connect('database.db')
 		cur = con.cursor()
 		cur.execute("insert into test(name,email,bill,phone,date) values(?,?,?,?,current_date)",(name,email,bill,phone))
 		cur.close()
 		con.commit()
-		con.close()	
+		con.close()
 
-		return render_template('index.html')
+		return temp_env.get_template('index.html').render()
+		#response_body = [f'{key}: {value}' for key, value in sorted(environ.items())]
+		#response_body = '\n'.join(response_body)
+		#return response_body
 
-
-@app.route('/**userdata**')
-def database():
-	con = sq.connect('database.db')
+def database(environ):
+	temp_env = Environment(loader = FileSystemLoader(os.path.abspath('template')))
+	con = sqlite3.connect('database.db')
 	cur = con.cursor()
 	cur.execute("select * from test")
 	data = cur.fetchall()
@@ -41,8 +80,12 @@ def database():
 	cur.close()
 	con.commit()
 	con.close()
-	return render_template('database.html',data=data)
+
+	return temp_env.get_template('database.html').render(data=data)
 
 
-if __name__=='__main__':
-	app.run()
+app.add_handlers('/',home)
+app.add_handlers('/9461884248',database)
+
+server = make_server('0.0.0.0',80,app = app)
+server.serve_forever()
